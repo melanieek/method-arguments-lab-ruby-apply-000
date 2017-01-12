ee9472a1dc0193d05764cfaf2aff83c7d87896b7
Melanie Kim <mkdesignsnyc@gmail.com>
2017-01-12 22:04:45 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+ruby
+def introduction (name,language)
+  "#{name}, #{language}"
+  end
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+
+introduction("Melanie", "Ruby")
